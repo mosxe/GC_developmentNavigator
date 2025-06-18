860ade831a29702b0a9c4511052124ea08dda8f6
AlekseevKV <AlekseevKV@gnivc.ru>
2025-06-18 12:18:36 +0300

    changed styles, hide buttonLink card

--- src/features/UserActivities/components/ActivityCard/index.tsx
+++ src/features/UserActivities/components/ActivityCard/index.tsx
@@ -8,6 +8,7 @@ import Modal from "@ff/ui-kit/lib/Modal";
 import Button from "@ff/ui-kit/lib/Button";
 import { createIpr } from "@/api";
 import { UserActivity } from "@/types";
+import clsx from "classnames";
 import styles from "./ActivityCard.module.scss";
 
 const ActivityCard: FunctionComponent<UserActivity> = (props): ReactElement => {
@@ -24,6 +25,10 @@ const ActivityCard: FunctionComponent<UserActivity> = (props): ReactElement => {
     isAssignable,
     progress,
   } = data;
+
+  const classNameCardContent = clsx(styles["activity-card__content"], {
+    [styles["activity-card__content-r"]]: !link,
+  });
 
   const handleClick = async () => {
     if (icon === "ipr" && isAssignable) {
@@ -93,7 +98,7 @@ const ActivityCard: FunctionComponent<UserActivity> = (props): ReactElement => {
     <>
       <div className={styles["activity-card"]}>
         <div className={styles["activity-card__wrapper"]}>
-          <div className={styles["activity-card__content"]}>
+          <div className={classNameCardContent}>
             <div className={styles["activity-card__icon"]}>
               <ActivityIcon iconName={icon} />
             </div>
@@ -112,32 +117,34 @@ const ActivityCard: FunctionComponent<UserActivity> = (props): ReactElement => {
               </span>
             </div>
           </div>
-          <div className={styles["activity-card__footer"]}>
-            <button
-              type="button"
-              className={styles["activity-card__btn"]}
-              onClick={handleClick}
-            >
-              <span>{buttonName}</span>
-              <svg
-                width="8.333008"
-                height="7.000000"
-                viewBox="0 0 8.33301 7"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
+          {link && (
+            <div className={styles["activity-card__footer"]}>
+              <button
+                type="button"
+                className={styles["activity-card__btn"]}
+                onClick={handleClick}
               >
-                <defs />
-                <path
-                  id="Vector"
-                  d="M0.66 4.18C0.29 4.18 0 3.88 0 3.51C0 3.14 0.29 2.84 0.66 2.84L6.06 2.84L4.36 1.13C4.1 0.87 4.1 0.45 4.36 0.19C4.62 -0.07 5.04 -0.07 5.3 0.19L8.13 3.04C8.39 3.3 8.39 3.72 8.13 3.98L5.3 6.8C5.04 7.06 4.62 7.06 4.36 6.8C4.1 6.54 4.1 6.12 4.36 5.86L6.04 4.18L0.66 4.18Z"
-                  fill="#494949"
-                  fillOpacity="1.000000"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+                <span>{buttonName}</span>
+                <svg
+                  width="8.333008"
+                  height="7.000000"
+                  viewBox="0 0 8.33301 7"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                  <defs />
+                  <path
+                    id="Vector"
+                    d="M0.66 4.18C0.29 4.18 0 3.88 0 3.51C0 3.14 0.29 2.84 0.66 2.84L6.06 2.84L4.36 1.13C4.1 0.87 4.1 0.45 4.36 0.19C4.62 -0.07 5.04 -0.07 5.3 0.19L8.13 3.04C8.39 3.3 8.39 3.72 8.13 3.98L5.3 6.8C5.04 7.06 4.62 7.06 4.36 6.8C4.1 6.54 4.1 6.12 4.36 5.86L6.04 4.18L0.66 4.18Z"
+                    fill="#494949"
+                    fillOpacity="1.000000"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          )}
         </div>
       </div>
       <Modal
